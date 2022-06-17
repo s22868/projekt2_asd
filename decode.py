e64a3cc0f0dac70710944767aa8847854ecbb443
Mateusz Budzynski <mati.leeder@gmail.com>
2022-06-17 17:54:36 +0200

    add decode

---
+++ decode.py
@@ -0,0 +1,19 @@
+def huffman_decode(encoded_data, huffman_tree):
+    tree_head = huffman_tree
+    decoded_output = []
+    for x in encoded_data:
+        if x == '1':
+            huffman_tree = huffman_tree.right
+        elif x == '0':
+            huffman_tree = huffman_tree.left
+        try:
+            if huffman_tree.left.symbol == None and huffman_tree.right.symbol == None:
+                pass
+        except AttributeError:
+            decoded_output.append(huffman_tree.symbol)
+            huffman_tree = tree_head
+
+    string = ''.join([str(item) for item in decoded_output])
+    return string
+
+# print("Po zdekodowaniu: ", huffman_decode(huffman_encode(string), nodes[0]))
